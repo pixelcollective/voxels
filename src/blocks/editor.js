443450301ba40939c0a9401ddc7ccb53676a2304
Kelly Mears <code@kellymears.me>
2018-12-16 00:22:21 -0800

    added generic header block and call-to-action block

--- src/blocks/editor.js
+++ src/blocks/editor.js
@@ -4,6 +4,7 @@ import './editor.scss';
 /** blocks */
 import './button/button';
 import './header/header';
+import './header/call-to-action';
 import './card/card';
 import './card/polaroid';
 import './card/horizontal';
